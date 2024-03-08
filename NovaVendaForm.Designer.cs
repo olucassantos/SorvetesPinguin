9d16f02b70899db121613a6c7dfc3efb83051538
Lucas Santos <lucas.santos@sp.senai.br>
2024-03-08 19:16:56 -0300

    Alteração do tamanho da imagem

--- NovaVendaForm.Designer.cs
+++ NovaVendaForm.Designer.cs
@@ -235,6 +235,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SorvetesPinguin.Properties.Resources.icebg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(674, 382);
             this.Controls.Add(this.txtCpfCliente);
             this.Controls.Add(this.lblValorTotal);
